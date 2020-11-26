f161a739de538ebf1cb13ccd8f3d3a311d093bc1
Nizo <38207294+theNizo@users.noreply.github.com>
2020-11-26 17:42:30 +0000

    Stop the rebooting

--- install-dvorakbynizo.sh
+++ install-dvorakbynizo.sh
@@ -4,4 +4,4 @@ cp /usr/share/X11/xkb/symbols/de de.bak # create backup
 rm de-nizo # delete potential old file, error can be expected
 wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/de-nizo # download the current file
 cp de-nizo /usr/share/X11/xkb/symbols/de # replace file
-reboot # reboot
+echo "All done, please log out and back in again"
