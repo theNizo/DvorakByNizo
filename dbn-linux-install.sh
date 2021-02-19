11af8ca988238eeb6bcb93ab82c6cba924c6a325
the_Nizo <38207294+theNizo@users.noreply.github.com>
2021-02-19 12:49:25 +0100

    Finished Script for now

--- dbn-linux-install.sh
+++ dbn-linux-install.sh
@@ -26,25 +26,82 @@ language
 while true; do
     read -p 'Selection [1/2/Q]:' lang
     case $lang in
-		#1)
-		#text=()
-		#break
-		#;;
+		1)
+		
+text=("Ungültige Eingabe"
+"Auswahl"
+
+"Dieses Skript installiert das \"Dvorak by
+Nizo (German)\" Layout, wodurch es Dvorak
+Type II überschreibt.
+
+[1] Mit FN Layer
+[2] Ohne FN Layer
+[Q] Stop"
+
+"Benutzen sie ein DE/einen WM, der/die
+key.type benötigt?
+
+GNOME    - beides geht
+KDE      - ?
+Xfce     - No
+LXDE     - ?
+MATE     - ?
+Cinnamon - ?
+i3       - Yes"
+
+"Es existiert bereits ein Backup.
+Soll dieses überschrieben werden?
+(Dieses Skript erstellt kein
+Backup eines Backups)"
+"Nein"
+"Ja"
+"Backup erstellen/überschreiben (automatisch
+ausgewählt, falls keines bisher
+existiert)"
+"Fortfahren?"
+"Erstelle Backup..."
+"Downloaden benötigter Dateien..."
+"Zusammenstellen der Datei..."
+"Das Layout ist installiert. Bitte ausloggen und wieder einloggen, danach das Layout auf \"Deutsch (Dvorak)\" stellen.")
+
+		break
+		;;
 		2)
 		
-text=("Invalid input" "Selection" "This script will install the Dvorak by Nizo (German) layout by overriding the standard german Dvorak layout.
+text=("Invalid input"
+"Selection"
+
+"This script will install the \"Dvorak by
+Nizo (German)\" layout by overriding
+Dvorak Type II
 
 [1] With FN Layer
 [2] Without FN Layer
-[Q] Quit" "Do you use a DE/WM that needs key.type?
+[Q] Quit"
 
-GNOME - choose either
-KDE - ?
-Xfce - No
-LXDE - ?
-MATE - ?
+"Do you use a DE/WM that needs key.type?
+
+GNOME    - choose either
+KDE      - ?
+Xfce     - No
+LXDE     - ?
+MATE     - ?
 Cinnamon - ?
-i3 - Yes" "There's already a backup. Override it?" "No" "Yes" "Create/override backup (chosen automatically if there is none)" "Is this correct?")
+i3       - Yes"
+
+"There's already a backup.
+Override it? (This script can't
+backup a backup)"
+"No"
+"Yes"
+"Create/override backup (chosen automatically
+if there is none)"
+"Is this correct?"
+"Creating backup..."
+"Downloading needed files..."
+"Building file..."
+"All done. Please log out and back in, then set the layout to German (Dvorak).")
 
 			break
             ;;
@@ -66,9 +123,13 @@ clear
 # 0 - no  #
 ###########
 
+#############
+# Ask stuff #
+#############
+
 # Ask for FN layer
 echo "${text[2]}"
-
+echo
 while true; do
     read -p "${text[1]} [1/2/Q]: " fn
     case $fn in
@@ -92,6 +153,7 @@ clear
 
 # Ask for key.type
 echo "${text[3]}"
+echo
 while true; do
     read -p "${text[1]} [y/n]: " yn
     case $yn in
@@ -108,20 +170,19 @@ while true; do
 	esac
 done
 
-clear
-
-backup=0
 # Check for backup
+backup=1
 if [ -f ~/.DvorakByNizo/de.bak ]
 then
+	echo
 	while true; do
 		read -p "${text[4]} [y/n]: " yn
 		case $yn in
 			[yY])
-			backup=1
 			break
 			;;
 			[nN])
+			backup=0
 			break
 			;;
 			*)
@@ -133,8 +194,6 @@ fi
 clear
 
 #Ask for confirmation
-echo "${text[4]}"
-echo
 echo "FN: ${text[$((5 + $fn))]}"
 echo "key.type: ${text[$((5 + $keytype))]}"
 echo "${text[7]}: ${text[$((5 + $backup))]}"
@@ -154,3 +213,57 @@ while true; do
 	esac
 done
 
+############
+# Do stuff #
+############
+
+# Backup
+echo
+if [ $backup -eq 1 ]
+then
+	echo "${text[9]}"
+	cp /usr/share/X11/xkb/symbols/de de.bak
+	
+fi
+
+# Collecting files, deleting old ones
+echo "${text[10]}"
+echo "1/3..."
+rm before &> /dev/null
+wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/before &> /dev/null
+echo "2/3..."
+rm after &> /dev/null
+wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/after &> /dev/null
+
+echo "3/3..."
+if [ $fn -eq 1 ]
+then
+	wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/dbn-FN &> /dev/null
+	mv dbn-FN dbn-standalone
+else
+	wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/dbn &> /dev/null
+	mv dbn dbn-standalone
+fi
+
+# Correcting key.type
+if [ $keytype -eq 1 ]
+then
+	sed -i 's/\/\/key.type/key.type/' dbn-standalone
+fi
+
+# Building file
+echo "${text[11]}"
+touch de
+# First should override, if it already exists, the other ones should append
+cat before > de
+cat dbn-standalone >> de
+cat after >> de
+
+# Install file
+sudo cp de /usr/share/X11/xkb/symbols
+echo
+echo "==========="
+echo "${text[12]}"
+echo "==========="
+echo
+exit 0
