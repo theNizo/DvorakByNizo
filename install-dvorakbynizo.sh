9c87c3cfe46489b77278c43a543d019b582f2627
the_Nizo <38207294+theNizo@users.noreply.github.com>
2021-02-19 11:24:32 +0100

    Linux variant more modular, enhancing bash script

--- install-dvorakbynizo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp /usr/share/X11/xkb/symbols/de de.bak # create backup
-rm de-nizo # delete potential old file, error can be expected
-wget https://raw.githubusercontent.com/theNizo/DvorakByNizo-German/master/Linux/de-nizo # download the current file
-cp de-nizo /usr/share/X11/xkb/symbols/de # replace file
-echo "All done, please log out and back in again"
